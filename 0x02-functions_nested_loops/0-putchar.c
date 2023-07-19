20a715694899837c2e3b878cdaa32b28ef22f881
AndzM89 <106753879+AndzM89@users.noreply.github.com>
2023-07-19 10:41:11 -0700

    Updated tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - print _putchar
+ * main - print_putchar
  *
- * return - 0
+ *
+ * Return: 0
 */
 
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
