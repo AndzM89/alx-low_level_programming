d8088e7022380d357a73bc1b2b8acd92ac271b8f
AndzM89 <106753879+AndzM89@users.noreply.github.com>
2023-07-20 09:44:33 -0700

    [B

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,15 @@
 */
 int print_sign(int n)
 {
-	{
 	if (n > 0)
 	 _putchar('+');
 	return (1);
-}
-	{
+	
 	else if (n == 0)
 	_putchar(48);
 	return (0);
-}
-	{
+	
 	else if (n < 0)
 	 _putchar('-');
 	return (-1);
-	}
 }
