9ef65c0999c75b0051e34277e023b2ce70529564
AndzM89 <106753879+AndzM89@users.noreply.github.com>
2023-07-21 00:37:40 -0700

    various

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n"); /* error */
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
 	while (i < 10)
 	{
 	putchar(i);
-	}
+	} /* loop error */
 
 	printf("Infinite loop avoided! \\o/\n");
 
