3967794b6467eb799472883751d3f8e0ed7cae55
AndzM89 <106753879+AndzM89@users.noreply.github.com>
2023-08-25 23:38:38 +0000

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
-global main
+global main
 extern printf
+
+section .data
+format db "Hello, Holberton", 0x0A
+
+section .text
 main:
 	mov edi, format
 	xor eax, eax
 	call printf
 	mov eax, 0
 	ret
-	format: db "Hello, Holberton\n",0
