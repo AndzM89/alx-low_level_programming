d37c6c2a7f0c4e25f6d4e5314506028c97b7d910
AndzM89 <106753879+AndzM89@users.noreply.github.com>
2023-09-13 10:59:19 -0700

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nnodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		nnodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+	
 	}
 
-	return (count);
+	return (nnodes);
 }
